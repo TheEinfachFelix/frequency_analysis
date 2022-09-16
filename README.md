8e48ede9fdf62ccb97ee82d64dc646ddeba4c7a5
TheEinfachFelix <the.einfach.felix@gmail.com>
2022-09-16 11:10:09 +0200

    bug fix

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # frequency_analysis
 this Script can find the frequency of letter in an string. can be used to decipher single and multi alphabet substitution encryption.
 
-the offset is a bit bugged
+the `ofset` (> 0)is the ofset for each iteration and `i`(>= 0) is the start of set.
+The `input` must be ab uppercase string that can but shoud not contain spaces 
--- b_zaler.py
+++ b_zaler.py
@@ -1,8 +1,8 @@
 input = 'LSRXUIBRSHQLTBWXBHSOZDBDJIIQSVOSYVSRSOWHSWZSMCUECNPPTWGWHIHWWIISMCSQTHLSSVVSLTWQHQLGWJINYTFJXBHTBHXSHTFICHWRVPJSWHSPJBKIFSINXSOKTUICZETGWIGMRVVJBHWSVPIWQSLPITISRSOWHAICGGWZMRVIGSVUWRSIRVGKTWWIYIXBIVSLTWQHQLGWJIOYHHYTTXTZRZORCRMTAICGGWZMRVIGSVUWRSIRVGKTWWIBMRVXPIGWOYUNYACIHSRKSVBCIRVXT'
-ofset = 3
+ofset = 3 # the general ofset (this can only be 1 or more not 0)
 abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 zählen = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-i = -3 #start offset (this is intersting, i have not found out the magic so...)
+i = 0 #start offset (0 is the first and so on)
 
 def finder(array, tofind) -> int:
     for element in range(len(array)):
@@ -17,13 +17,11 @@ def esc_print():
 
 def main():
     global i
-    length = len(input) 
     while i <= len(input):
-        
+        zählen[finder(abc, input[i])] += 1
         i += ofset
         if i >= len(input):
                 esc_print()  
-        zählen[finder(abc, input[i])] += 1
     esc_print()
 
 if __name__ == '__main__':
